fc3aebcab2da4b4fd211595d5957ed06edd63432
Stefano Manzini <stefano.manzini@gmail.com>
2021-03-23 13:23:13 +0100

    Update __init__.py

--- restring/__init__.py
+++ restring/__init__.py
@@ -1,4 +1,16 @@
 # TODO: installation via Pip needs to be fixed
-__version__ = "0.00.0"
+__version__ = "0.1.1"
 
-from restring.restring import *
+# the following tests if I can import stuff from restring.py
+from .restring import files
+
+# the following tests if I can import stuff from restring.py
+from restring.restring import working_directory
+
+# the following tests if I can import stuff from gears.py, and where it will end up
+from .gears import manzlog
+
+# the following tests if I can import stuff from gears.py, and where it will end up
+from gears.gears import get_dirs
+
+__all__ = ("files", "working_directory", "manzlog", "get_dirs")
