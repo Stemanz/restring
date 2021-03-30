7f70622c4aef1bc56ff7cd22b2af098ae287ff3a
Stefano Manzini <stefano.manzini@gmail.com>
2021-03-30 10:56:35 +0200

    Update __init__.py

--- restring/__init__.py
+++ restring/__init__.py
@@ -1,24 +1,12 @@
 # TODO: installation via Pip needs to be fixed
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 
-# the following tests if I can import stuff from restring.py
+# the following tests if and how I can import stuff
 #from .restring import files # this works
+#from restring.restring import working_directory #this also works
+#from .gears import manzlog # this works
+#from restring.gears import get_dirs # this also works
 
-# the following tests if I can import stuff from restring.py
-from restring.restring import working_directory, files #this also works
+from restring.restring import restring_gui
 
-# the following tests if I can import stuff from gears.py, and where it will end up
-from .gears import manzlog # this works
-
-# the following tests Giorgio's advice about importing
-from restring.gears import get_dirs
-
-# the following tests if I can import stuff from gears.py, and where it will end up
-#from gears.gears import get_dirs # this fails:
-#Traceback (most recent call last):
-#  File "<stdin>", line 1, in <module>
-#  File "/Users/manz/opt/anaconda3/envs/py38/lib/python3.8/site-packages/restring/__init__.py", line 14, in <module>
-#    from gears.gears import get_dirs
-#ModuleNotFoundError: No module named 'gears'
-
-__all__ = ("files", "working_directory", "manzlog", "get_dirs")
+__all__ = ("restring_gui")
